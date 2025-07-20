b007e500a0d5f39a05177a5247164436ffc4c102
yeetboi-1010 <lklisara28@gmail.com>
2025-07-19 21:26:04 -0700

    Aaaand I actually fixed it this time
    The last "fix" didn't work. But I just fixed it now! :)

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -3,7 +3,7 @@ extends Area2D
 signal hp_reduction
 
 func _on_body_entered(body: Node2D) -> void:
-	if body.get_parent() is Player:
+	if body.is_in_group("Player"):
 		print("You Died!")
 		emit_signal("hp_reduction")
 
